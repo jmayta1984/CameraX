43e3abd061e0c82176fbd40151e57d4f621366fa
jmayta1984 <Monica$2017>
2020-09-12 08:48:45 -0500

    Example with CameraX

--- app/src/main/java/pe/edu/upc/camerax/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/camerax/MainActivity.kt
@@ -3,17 +3,21 @@ package pe.edu.upc.camerax
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.security.Permission
-import java.util.jar.Manifest
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     var imageCapture: ImageCapture? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISONS,
+                this, REQUIRED_PERMISSIONS,
                 REQUEST_CODE_PERMISSION
             )
         }
@@ -60,17 +64,42 @@ class MainActivity : AppCompatActivity() {
 
     private fun startCamera() {
 
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(pvCamera.createSurfaceProvider())
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector,preview)
+            }  catch(exc: Exception) {
+                Log.e("MainActivity", "Falló la operación de binding",exc)
+
+            }
+        }, ContextCompat.getMainExecutor(this))
+
     }
 
     //Método que verifica si se otorgaron los permisos
-    private fun allPermissonsGranted(): Boolean = REQUIRED_PERMISONS.all {
+    private fun allPermissonsGranted(): Boolean = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it
         ) == PackageManager.PERMISSION_GRANTED
     }
 
     companion object {
-        val REQUIRED_PERMISONS = arrayOf(android.Manifest.permission.CAMERA)
-        val REQUEST_CODE_PERMISSION = 10
+        val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+        const val REQUEST_CODE_PERMISSION = 1
     }
 }
